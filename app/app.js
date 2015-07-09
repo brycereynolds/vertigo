d2c8a4c18555662b5372a38bda2a9a4fb10d3f5f
Bryce Reynolds <brycereynoldsdesign@gmail.com>
2015-07-09 16:15:38 -0700

    Testing with CircleCI

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 "use strict";
 var server = require("./server.js");
+var util = require('util');
 
 server.route({
   method: 'POST',
   path: '/playbook',
   handler: function (request, reply) {
     console.log("IN PLAYBOOK!");
-    console.log(request.payload);
+
+    console.log(util.inspect(request.payload, false, null));
+
     reply().code(200);
   }
 });
